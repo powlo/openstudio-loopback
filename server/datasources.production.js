570c55d6fcaa57c0749775d452a76d9a968c03fe
powlo <paulcbackhouse@gmail.com>
2017-01-05 18:58:12 +0000

    Use mlab to save disk quota

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,10 +2,10 @@ var path = require('path');
 
 module.exports = {
     "MongoDB": {
-        hostname: process.env.OPENSHIFT_MONGODB_DB_HOST,
-        port: process.env.OPENSHIFT_MONGODB_DB_PORT,
-        user: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
-        password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD
+        hostname: process.env.OPENSHIFT_MONGOLAB_DB_HOST,
+        port: process.env.OPENSHIFT_MONGOLAB_DB_PORT,
+        user: process.env.OPENSHIFT_MONGOLAB_DB_USERNAME,
+        password: process.env.OPENSHIFT_MONGOLAB_DB_PASSWORD
     },
     "filestore": {
         "root": path.resolve(process.env.OPENSHIFT_DATA_DIR, "upload")
