3bc114aa731815a9f14df8fa7c5f854037763e07
powlo <paulcbackhouse@gmail.com>
2017-09-08 17:29:10 +0100

    remove console comments.

--- client/app/scripts/controllers/event_list.js
+++ client/app/scripts/controllers/event_list.js
@@ -8,8 +8,6 @@ angular.module('openstudioAngularApp')
       function getEvents(filter) {
         Event.find(filter,
           function(objs) { /*success*/
-            console.log("Events: " + objs.length);
-            console.log(objs[0]);
             $scope.events = objs;
           },
           function(err) { /*error*/
@@ -25,8 +23,6 @@ angular.module('openstudioAngularApp')
       };
 
       $scope.$watch('filter.date', function(newVal){
-        console.log("date changed!");
-        console.log("New Val: " + newVal);
         const today = new Date();
         today.setUTCHours(0, 0, 0, 0);
         const tomorrow = new Date();
--- client/dist/scripts/scripts-a0d886f4aa.js
+++ client/dist/scripts/scripts-a0d886f4aa.js
@@ -6483,8 +6483,6 @@ angular.module('openstudioAngularApp')
       function getEvents(filter) {
         Event.find(filter,
           function(objs) { /*success*/
-            console.log("Events: " + objs.length);
-            console.log(objs[0]);
             $scope.events = objs;
           },
           function(err) { /*error*/
@@ -6500,8 +6498,6 @@ angular.module('openstudioAngularApp')
       };
 
       $scope.$watch('filter.date', function(newVal){
-        console.log("date changed!");
-        console.log("New Val: " + newVal);
         const today = new Date();
         today.setUTCHours(0, 0, 0, 0);
         const tomorrow = new Date();
