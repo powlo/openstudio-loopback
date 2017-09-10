0f6f96999a5ccfc959d405afdcac4ea529a7a916
powlo <paulcbackhouse@gmail.com>
2017-09-10 15:15:46 +0100

    Added facebook login.

--- server/server.js
+++ server/server.js
@@ -2,8 +2,24 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
 
 var app = module.exports = loopback();
+var passport_config = new PassportConfigurator(app);
+
+// Load the provider configurations
+var config = {};
+try {
+ config = require('./providers.json');
+} catch(err) {
+  console.log(err);
+ console.error('Please configure your passport strategy in `providers.json`.');
+ console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
+ process.exit(1);
+}
+
+// Initialize passport
+passport_config.init();
 
 app.start = function() {
   // start the web server
@@ -34,3 +50,17 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+// Set up related models
+passport_config.setupModels({
+ userModel: app.models.StudioUser,
+ userIdentityModel: app.models.UserIdentity,
+ userCredentialModel: app.models.UserCredential
+});
+
+// Configure passport strategies for third party auth providers
+for(var s in config) {
+ var c = config[s];
+ c.session = c.session !== false;
+ passport_config.configureProvider(s, c);
+}
